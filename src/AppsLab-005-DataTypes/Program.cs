ea817d474a817ef1bbcfc12c2c7c855eb0a63ff3
Radovan578 <radovan.tanuska@gmail.com>
2025-09-10 10:05:13 +0200

    uloha 005 - skusanie premennych

--- src/AppsLab-005-DataTypes/Program.cs
+++ src/AppsLab-005-DataTypes/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isAdult = false;
+            Console.WriteLine(isAdult);
+
+            int myAge = 15;
+            Console.WriteLine(myAge);
+
+            float pi = 3.14f;
+            Console.WriteLine(pi);
+
+            string name = "Rado";
+            Console.WriteLine(name);
+
+            char gender = 'M';
+            Console.WriteLine(gender);
+
+        }
+    }
+}
