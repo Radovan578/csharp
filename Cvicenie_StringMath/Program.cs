3b9ea95797ae1b3bd90e806079149c5ab334ce4d
Radovan578 <radovan.tanuska@gmail.com>
2025-10-15 12:30:57 +0200

    Praca s metodami

---
+++ Cvicenie_StringMath/Program.cs
@@ -0,0 +1,68 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Cvicenie_StringMath
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            string message = "Hello \nWorld!";
+            Console.WriteLine(message);               //vysledok: Hello
+                                                      //   World!
+            string name = "John";
+            string greeting = $"Hello, {name} {5 + 10} !";
+            string greetingOld = "Hello, " + "John" + (5 + 10) + "!";
+            Console.WriteLine(greeting);
+            Console.WriteLine(greetingOld);
+            */
+
+            /*
+            string name = "John";
+            Console.WriteLine(name.ToUpper());
+            */
+
+            /*
+            string name = "Test123Skuska";
+            bool contains = name.Contains("123");
+            Console.WriteLine(contains + "w");
+            */
+
+            /*
+            string name = "John123";
+            name = name.Replace("John123", "Jan");
+            Console.WriteLine(name);
+            */
+
+            /*
+            int sum = 500;
+            double power = Math.Max(4,8);    
+            Console.WriteLine(power);
+            */
+            Console.WriteLine("Zadaj prve cislo:");
+            int number = int.Parse(Console.ReadLine());
+            Console.WriteLine("Zadaj druhe cislo:");
+            int number2 = int.Parse(Console.ReadLine());
+            int sum = Scitanie(5,10,4,number,number2);
+            Console.WriteLine(sum);
+        }
+        public static int Scitanie(int a, int b, int c, int d, int f)
+        {
+            int g = a + b + c + d + f;
+            return g;
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+    }
+}
+
---
+++ Cvicenie_Subory/Program.cs
@@ -0,0 +1,118 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Cvicenie_Subory
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] text = File.ReadAllLines("People_100.csv");
+            /*
+            MoneyCountAverage(text);
+            */
+            /*
+            WriteRodneCislo(text);
+            */
+            /*
+            MinMoneyCount(text);
+            */
+            /*
+            List<string> mojZoznam = PeopleUnder05M(text);
+            */
+
+            PeopleUnder05M(text);
+        }
+        /*
+        public static List<string> PeopleUnder05M(string[] text)
+        {
+            //pomocny list(nie pole) kde budeme pridavat osoby => list stringov
+            foreach (string line in text.Skip(1))
+            {
+                //splitnut riadok
+                //najst kolko ma na ucte penazi
+                //ak ma menej ako 500 000 pridat do listu 
+
+            }
+               //vrati ti list
+        }
+        */
+
+
+        public static void MoneyCountAverage(string[] text)
+        {
+            int sum = 0;
+            foreach (string line in text.Skip(1))
+            {
+                //Martin,Urban, 690602/2315,Presov,463102,slobodny
+                string[] splits = line.Split(";");
+                //prekonvertovanie hodnoty z retazca na cislo
+                int accountValue = int.Parse(splits[4]);
+                //scitanie int hodnoty so sum-om
+                sum += accountValue;
+
+            }
+            Console.WriteLine(sum / (text.Count() - 1));
+
+
+
+        }
+
+        public static void WriteRodneCislo(string[] text)
+        {
+            foreach (string line in text.Skip(1))
+            {
+                string[] splits = line.Split(";");
+
+                string RodneCisla = splits[2];
+                Console.WriteLine(RodneCisla);
+            }
+
+        }
+        public static void MinMoneyCount(string[] text)
+        {
+            int minValue = 9999999;
+            string minValuePerson = "";
+            foreach (string line in text.Skip(1))
+            {
+                string[] splits = line.Split(";");
+
+                int accountValue = int.Parse(splits[4]);
+
+                if (accountValue < minValue)
+                {
+                    minValue = accountValue;
+                    minValuePerson = splits[0] + " " + splits[1];
+                }
+            }
+            Console.WriteLine(minValuePerson);
+        }
+        public static void PeopleUnder05M(string[] text)
+        {
+
+            int minValue = 500000;
+            string PeopleUnder = "";
+            foreach (string line in text.Skip(1))
+            {
+                string[] splits = line.Split(";");
+
+                int accountValue = int.Parse(splits[4]);
+
+                if (accountValue < minValue)
+                {
+                    minValue = accountValue;
+                    PeopleUnder = splits[0] + " " + splits[1];
+                    Console.WriteLine(PeopleUnder);
+                }
+            }
+            
+
+
+        }
+
+
+
+
+    }
+}
