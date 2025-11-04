500d2df3c6608ef6110a63bed3ad918427acca41
Radovan578 <radovan.tanuska@gmail.com>
2025-11-04 18:35:55 +0100

    uloha s BattleSim

---
+++ Cvicenie_BattleSimulator/Monster.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cvicenie_BattleSimulator
+{
+    public class Monster
+    {
+        public string RaceType { get; set; }   //Monster race type (e.g., Goblin, Orc, Troll)
+        public int HP { get; set; }    //Health points
+        public int DMG { get; set; }    //Damage
+
+        public Monster(string raceType, int hP, int dMG)
+        {
+            RaceType = raceType;
+            HP = hP;
+            DMG = dMG;
+        }
+
+        public void MonsterAttack(Hero hero)
+        {   if (hero.SHD > DMG)
+            {
+                hero.HP = hero.HP - 0;
+            }
+            else if (hero.SHD < DMG)
+            {
+                hero.HP = (hero.SHD + hero.HP) - DMG;
+            }
+
+
+
+        }
+    }
+}
---
+++ Cvicenie_BattleSimulator/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.Threading;
+
+namespace Cvicenie_BattleSimulator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Hero ourHero = new Hero();
+            Monster monster1 = new Monster("Goblin", 150, 50);
+
+
+            while (true)
+            {
+                //Hero dostal utok od monstra
+                monster1.MonsterAttack(ourHero);
+                Console.WriteLine("HERO:HP " + ourHero.HP);
+
+                //Monster dostal utok od hrdinu
+                bool wasAttack = ourHero.HeroAttack(monster1);
+                if (wasAttack)
+                {
+                    Console.WriteLine("MONSTER:HP " + monster1.HP);
+                }
+                else
+                {
+                    Console.WriteLine("---Not enough energy to attack! Restoring energy...");
+                    Console.WriteLine("HERO:energy " + ourHero.ENG);
+                }
+
+                if (ourHero.HP <= 0)
+                {
+                    Console.WriteLine("Hero is dead!");
+                    break;
+                }
+
+                if (monster1.HP <= 0)
+                {
+                    Console.WriteLine("Monster is dead!");
+                    break;
+                }
+
+
+
+            }
+        }
+    }
+}
+
