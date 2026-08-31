a4074c255f1cc3d1fae758d2cd915494ed4a4cbc
Radovan578 <radovan.tanuska@gmail.com>
2026-08-31 15:57:58 +0200

    mini kalkulacka cez prazdniny

---
+++ miniKalkulacka/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace miniKalkulacka
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int vysledok = 0;
+
+            Console.WriteLine("Napis prve cislo: ");
+            int pCislo = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Napis druhe cislo: ");
+            int dCislo = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Co s nimi chces spravit? ( +, -, *, / )");
+            string znak = Console.ReadLine();
+
+            if (znak == "+")
+            {
+                vysledok = pCislo + dCislo;
+            }
+            else if (znak == "-")
+            {
+                vysledok = pCislo - dCislo;
+            }
+            else if (znak == "*")
+            {
+                vysledok = pCislo * dCislo;
+            }
+            else if (znak == "/")
+            {
+                vysledok = pCislo / dCislo;
+            }
+
+            Console.WriteLine(pCislo + znak + dCislo + " = " + vysledok);
+            
+        }
+    }
+}
